90cd0abb939b1bff21788488a2ffccdb0b9ba794
Shubhasheesh-2020 <ashuprakash@gmail.com>
2022-10-05 03:18:49 +0800

    fixed footer to bottom of page

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-    <footer class="footer">
-        <p class="text-center">Copyright (c) 2022</p>
+    <footer class="container-fluid text-center p-1 fixed-bottom bg-danger">
+        <p>Copyright (c) 2022</p>
     </footer>
 </template>
+
+
