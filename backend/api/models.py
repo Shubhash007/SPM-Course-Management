570e9fc5db699ce93818fe3c7a4e02383b8dedcb
Shubhasheesh-2020 <ashuprakash@gmail.com>
2022-10-07 11:44:55 +0800

    added db tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Role(models.Model):
-    """Model definition for Role."""
-    Role_ID = models.IntegerField(primary_key=True, null=False)
-    Role_Name = models.CharField(null=False,max_length=20)
+class User_Role(models.Model):
+    """Model definition for User_Role."""
+    User_Role_ID = models.IntegerField(primary_key=True, null=False)
+    User_Role_Name = models.CharField(null=False,max_length=20)
     
 
     class Meta:
-        """Meta definition for Role."""
-        verbose_name = 'Role'
+        """Meta definition for User_Role."""
+        verbose_name = 'User_Role'
 
     def __str__(self):
-        return f'{self.Role_ID} : {self.Role_Name}'
-
-
+        return f'{self.User_Role_ID} : {self.User_Role_Name}'
+###############################################################################
 class Staff(models.Model):
     """Model definition for Staff."""
     Staff_ID = models.IntegerField(primary_key=True, null=False)
@@ -22,15 +21,30 @@ class Staff(models.Model):
     Staff_LName = models.CharField(null=False,max_length=50)
     Dept = models.CharField(null=False,max_length=50)
     Email = models.EmailField(null=False,max_length=50)
-    Role = models.ForeignKey(Role,on_delete=models.CASCADE)
+    Role = models.ForeignKey(User_Role,on_delete=models.CASCADE)
     class Meta:
         """Meta definition for Staff."""
         verbose_name =  'Staff'
 
     def __str__(self):
         return f"{self.Staff_ID}"
+##############################################################################
+class Skill(models.Model):
+    """Model definition for Skill."""
+    Skill_ID = models.IntegerField(primary_key=True, null=False)
+    Skill_Name = models.CharField(null=False,max_length=50) 
+    Skill_Desc = models.CharField(max_length=255)
+
+    class Meta:
+        """Meta definition for Skill."""
+        verbose_name = 'Skill'
+
+    def __str__(self):
+        """Unicode representation of Skill."""
+        pass
 
 
+################################################################################
 class Course(models.Model):
     """Model definition for Courses."""
     Course_ID = models.CharField(primary_key=True, null=False,max_length=20)
@@ -48,7 +62,7 @@ class Course(models.Model):
 
     def __str__(self):
         return f'{self.Course_ID} : {self.Course_Name},{self.Course_Status},{self.Course_Category}'
-
+################################################################################
 class Registration(models.Model):
     """Model definition for Registration."""
     # TODO: Define fields here
@@ -65,4 +79,19 @@ class Registration(models.Model):
     def __str__(self):
         """Unicode representation of Registration."""
         pass
+###################################################################################
+class JobRole(models.Model):
+    """Model definition for Job_Role."""
+    # TODO: Define fields here
+    Job_Role_ID = models.IntegerField(primary_key=True, null=False)
+    Job_Role_Desc = models.CharField(max_length=255)
 
+
+    class Meta:
+        """Meta definition for Job_Role."""
+        verbose_name = 'Job_Role'
+
+    def __str__(self):
+        """Unicode representation of JobRole."""
+        pass
+###################################################################################
