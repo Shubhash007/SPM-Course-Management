6ea636ab97fe2545723534c19fd837aee8c3bbd9
liqipotato <liqipotato@gmail.com>
2022-10-16 17:00:44 +0800

    changed to edit button

--- frontend/src/components/DeleteLJ.vue
+++ frontend/src/components/DeleteLJ.vue
@@ -1,63 +1,108 @@
 <template>
     <div class="accordion-item">
-            <h2 class="accordion-header" :id="'heading'+ num">
+        <h2 class="accordion-header" :id="'heading'+ num">
             <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target="'#collapse' + num" aria-expanded="true" :aria-controls="'collapseOne' + num">
                 {{role}}
             </button>
-            
         </h2>
+
         <div :id="'collapse' + num" class="accordion-collapse collapse" :aria-labelledby="'heading'+ num" data-bs-parent="#accordionExample">
             <div class="accordion-body">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item" v-for="value, key in skills">{{key}}
-                            <ol>
-                                <li v-for="skill in value">
-                                    {{skill}}
-                                    <button type="submit" class="btn btn-sm delete-button" @click="deleteCourse">Delete</button>
-                                </li>
-                            </ol>
-                            <br>
+                    <li class="list-group-item" v-for="value, skill in skills">
+                        <div>
+                            <div class="row">
+                                <div class="col-10">
+                                    {{skill}} 
+                                </div>
+                                <div class="col-2">
+                                    <!-- <button type="submit" class="btn btn-sm pink-button" @click="deleteCourse">Edit</button> -->
+                                    <div class="modal fade" id="edit-button" aria-hidden="true" aria-labelledby="first-modal" tabindex="-1">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="first-modal">Edit Courses</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div v-for="course in value">
+                                                    <input type="checkbox" v-model="courses" name="courses[]" v-bind:value="course"> {{course}}
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button class="btn pink-button" data-bs-target="#second-modal" data-bs-toggle="modal">Confirm</button>
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                        <div class="modal fade" id="second-modal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Confirmation</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                You have successfully edited your courses. 
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button class="btn pink-button" data-bs-target="#edit-button" data-bs-toggle="modal">Back</button>
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                        <a class="btn btn-sm pink-button" data-bs-toggle="modal" href="#edit-button" role="button">Edit</a>
+                                </div>
+                            </div>
+                            <div>
+                                <ol>
+                                    <li v-for="course in value">
+                                        {{course}}
+                                        <!-- <button type="submit" class="btn btn-sm pink-button" @click="deleteCourse">Delete</button> -->
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                        <br>
                     </li>
                 </ul>
                 <button type="button" class="btn btn-outline-danger btn-md">Delete Learning Journey</button>
             </div>
         </div>
-    </div> 
+    </div>     
+</template>
     
     
-    </template>
-    
-    
-<script setup>
+<script setup>    
     const props = defineProps({
         num: Number,
-        role:String,
-        skills:Object
+        role: String,
+        skills: Object,
+        courses: Object,
     })
-    
 </script>
 
     
-    <style scoped>
-      #search{
-          border-color: #2F2FFA;
-      }
-      
-      #search::placeholder{
-          color: #2F2FFA;
-      }
+<style scoped>
+    #search{
+        border-color: #2F2FFA;
+    }
     
-      .search-textbox:focus{
-          box-shadow: #F64C72;
-      }
-      
-      .delete-button{
+    #search::placeholder{
+        color: #2F2FFA;
+    }
+
+    .search-textbox:focus{
+        box-shadow: #F64C72;
+    }
+    
+    .pink-button{
+    background-color: #F64C72;
+    color: white;
+    }
+
+    .pink-button:hover{
         background-color: #F64C72;
-        color: white;
-      }
-    
-      .delete-button:hover{
-          background-color: #F64C72;
-          color: black;
-      }
-    </style>
+        color: black;
+    }
+</style>
