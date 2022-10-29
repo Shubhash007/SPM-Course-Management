2dc4fca21e25e268dc6b82759a743501093eddfb
liqipotato <liqipotato@gmail.com>
2022-10-29 18:40:08 +0800

    added manager home page and cleaned up vue files in directory

--- frontend/src/views/admin/ViewRoles.vue
+++ frontend/src/views/admin/ViewRoles.vue
@@ -4,88 +4,92 @@
         <div class="row g-3" style="margin: 20px 50px;">
             <div class="col-auto">
                 <label for="search" class="visually-hidden">Search</label>
-                <input type="text" @keyup="searchFunction" v-model="keyword" class="form-control search-textbox" id="search" placeholder="Search skill here...">
+                <input type="text" @keyup="searchFunction" v-model="keyword" class="form-control search-textbox" id="search" placeholder="Search by...">
             </div>
         </div>
         <table class="table table-responsive" style="width: 75%; border-color: #2F2FFA; margin: 20px 60px;" border="#2F2FFA">
             <thead>
                 <tr>
                 <th scope="col">S/N</th>
-                <th scope="col">Skill Name</th>
-                <th scope="col">Courses</th>
-                <th></th>
+                <th scope="col">Staff Name</th>
+                <th scope="col">Job Role</th>
                 </tr>
             </thead>
             <tbody id="tbody">
-                <tr v-for="(skill, index) in skills">
+                <tr v-for="(employee, index) in employees">
                     <th scope="row" style="color: #2F2FFA">{{index+1}}</th>
-                    <td>{{ skill.Skill_Name}}</td> 
-                    <td>{{ skill.courses.toString() }}</td>
-                    <td><EditSkill :skillID="skill.Skill_ID" :skillName="skill.Skill_Name" :allSkills="skills"></EditSkill></td>
+                    <td>{{ employee.Staff_FName}} {{employee.Staff_LName}}</td>
+                    <!-- <td><router-link :to="{name: 'StaffProfile', params: {slug: employee.StaffID}}">{{ employee.Staff }}</router-link></td> -->
+                    <td>{{ employee.Dept}}</td> 
                 </tr>
             </tbody>
         </table>
     </div>
 </template>
 <script setup>
-    import NavBar from '../components/NavBar.vue';
-    import EditSkill from '../components/editSkillModal.vue';
+    import NavBar from '../../components/NavBar.vue';
 </script>
 <script>
     import axios from "axios";
-
     export default{
         data(){
             return{
-                skills:[],
+                employees: [],
+                category: "Category",
                 keyword: "",
                 hasSearch: false,
                 returnData: []
             }
         }, methods:{
+
+
             onload: function(){
-                axios.get('/skill/')
+            axios.get('/staff/')
             .then(response => {
                 // this.course = response.data.data;
-                this.skills = response.data
-                console.log(this.skills)
+                this.employees = response.data
+                console.log(response.data)
+                // console.log(this.employees[10].courses);
             })
             .catch(error => alert(error)) 
-            },    
+            },  
+
+
+
             searchFunction: function () {
-                var input, filter, tbody, tr, tdName, i, txtValue;
+                var input, filter, table, tbody, tr, tdName, tdRoles, i, txtValue;
                 input = document.getElementById("search");
                 filter = input.value.toUpperCase();
+                table = document.getElementById("roles");
                 tbody = document.getElementById("tbody");
                 tr = tbody.getElementsByTagName("tr");
 
                 for (i = 0; i < tr.length; i++) {
                     tdName = tr[i].getElementsByTagName("td")[0];
-                
-                    if (tdName) {
-                    txtValue = tdName.textContent;
+                    tdRoles = tr[i].getElementsByTagName("td")[1];
+                    if (tdName && tdRoles) {
+                    txtValue = [tdName.textContent, tdRoles.textContent];
                     console.log(txtValue)
-                    if ((txtValue.toUpperCase().indexOf(filter) > -1)) {
+                    if ((txtValue[0].toUpperCase().indexOf(filter) > -1) || (txtValue[1].toUpperCase().indexOf(filter) > -1)) {
                         tr[i].style.display = "";
                     } else {
                         tr[i].style.display = "none";
                     }
                     }
                 }
-                }
+            }
+
         },
+
         mounted(){
             this.onload()
         },
         created() {
             this.onload()
         },
+        
     }
-
-
-
 </script>
-
 <style scoped>
     #search{
         border-color: #2F2FFA;
@@ -99,7 +103,41 @@
         box-shadow: #F64C72;
     }
 
+    .category-dropdown{
+        border-color: #F64C72;
+        color: #F64C72;
+    }
+
+    .category-dropdown:hover{
+        border-color: #F64C72;
+        color: black
+    }
+
+    .category-dropdown:focus{
+        border-color: #F64C72;
+        color: black;
+        box-shadow: none;
+    }
+    
+    .search-button{
+        background-color: #F64C72;
+        color: white;
+    }
+
+    .search-button:hover{
+        background-color: #F64C72;
+        color: black;
+    }
     td{
         color: #2F2FFA;
+    }
+
+    a{
+        color: #2F2FFA;
+        text-decoration: none;
+    }
+
+    a:hover{
+        color:#F64C72;
     }
 </style>
---
+++ frontend/src/views/admin/ViewSkills.vue
@@ -0,0 +1,106 @@
+<template>
+    <NavBar></NavBar>
+    <div style="min-height: 80vh;">
+        <div class="row g-3" style="margin: 20px 50px;">
+            <div class="col-auto">
+                <label for="search" class="visually-hidden">Search</label>
+                <input type="text" @keyup="searchFunction" v-model="keyword" class="form-control search-textbox" id="search" placeholder="Search skill here...">
+            </div>
+        </div>
+        <table class="table table-responsive" style="width: 75%; border-color: #2F2FFA; margin: 20px 60px;" border="#2F2FFA">
+            <thead>
+                <tr>
+                <th scope="col">S/N</th>
+                <th scope="col">Skill Name</th>
+                <th scope="col">Courses</th>
+                <th></th>
+                </tr>
+            </thead>
+            <tbody id="tbody">
+                <tr v-for="(skill, index) in skills">
+                    <th scope="row" style="color: #2F2FFA">{{index+1}}</th>
+                    <td>{{ skill.Skill_Name}}</td> 
+                    <td>{{ skill.courses.toString() }}</td>
+                    <td><EditSkill :skillID="skill.Skill_ID" :skillName="skill.Skill_Name" :allSkills="skills"></EditSkill></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script setup>
+    import NavBar from '../../components/NavBar.vue';
+    import EditSkill from '../../components/admin/editSkillModal.vue';
+</script>
+
+<script>
+    import axios from "axios";
+
+    export default{
+        data(){
+            return{
+                skills:[],
+                keyword: "",
+                hasSearch: false,
+                returnData: []
+            }
+        }, methods:{
+            onload: function(){
+                axios.get('/skill/')
+            .then(response => {
+                // this.course = response.data.data;
+                this.skills = response.data
+                console.log(this.skills)
+            })
+            .catch(error => alert(error)) 
+            },    
+            searchFunction: function () {
+                var input, filter, tbody, tr, tdName, i, txtValue;
+                input = document.getElementById("search");
+                filter = input.value.toUpperCase();
+                tbody = document.getElementById("tbody");
+                tr = tbody.getElementsByTagName("tr");
+
+                for (i = 0; i < tr.length; i++) {
+                    tdName = tr[i].getElementsByTagName("td")[0];
+                
+                    if (tdName) {
+                    txtValue = tdName.textContent;
+                    console.log(txtValue)
+                    if ((txtValue.toUpperCase().indexOf(filter) > -1)) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                    }
+                }
+                }
+        },
+        mounted(){
+            this.onload()
+        },
+        created() {
+            this.onload()
+        },
+    }
+
+
+
+</script>
+
+<style scoped>
+    #search{
+        border-color: #2F2FFA;
+    }
+    
+    #search::placeholder{
+        color: #2F2FFA;
+    }
+
+    .search-textbox:focus{
+        box-shadow: #F64C72;
+    }
+
+    td{
+        color: #2F2FFA;
+    }
+</style>
