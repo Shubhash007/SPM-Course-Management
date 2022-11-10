cc19cee27cfe755c2d00d2050d1a005cab6846af
liqipotato <89569278+liqipotato@users.noreply.github.com>
2022-11-10 18:25:30 +0800

    UI fixes part 2

--- frontend/src/components/admin/editRoleDetailsModal.vue
+++ frontend/src/components/admin/editRoleDetailsModal.vue
@@ -7,7 +7,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Role</h1>
+            <h1 class="modal-title fs-5" id="staticBackdropLabel" style="margin-top: 0px; color:#d8648b;">Edit Role</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
